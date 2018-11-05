faef5dd14d1ff0b0b516a019921fedcf95daf9d8
Janne Valkealahti <janne.valkealahti@gmail.com>
2018-11-05 12:40:45 +0000

    stash

--- ci/scripts/build-project.sh
+++ ci/scripts/build-project.sh
@@ -7,41 +7,76 @@ repository=$(pwd)/distribution-repository
 buildversion=`date '+%Y-%m-%d-%H-%M-%S'`
 
 pushd git-repo > /dev/null
-# pushd $BASE_PATH > /dev/null
+# with no base path, don't try to change dir
+if [ -n "$BASE_PATH" ] && pushd $BASE_PATH > /dev/null
 echo $ARTIFACTORY_PASSWORD | docker login -u $ARTIFACTORY_USERNAME --password-stdin springsource-docker-private-local.jfrog.io
 
-export SPRING_CLOUD_DEPLOYER_CLOUDFOUNDRY_URL=https://api.sys.needles.cf-app.com
-export SPRING_CLOUD_DEPLOYER_CLOUDFOUNDRY_ORG=bamboo
-export SPRING_CLOUD_DEPLOYER_CLOUDFOUNDRY_SPACE=test
-export SPRING_CLOUD_DEPLOYER_CLOUDFOUNDRY_DOMAIN=app.needles.cf-app.com
+CLEANCMD1="./run.sh -t -s -d -dv"
+RUNCMD="./run.sh"
+CLEANCMD2="./run.sh"
+
+if [ -n "$PLATFORM" ]
+then
+  CLEANCMD1+=" -p $PLATFORM"
+  CLEANCMD2+=" -p $PLATFORM"
+  RUNCMD+=" -p $PLATFORM"
+fi
+
+if [ -n "$BINDER" ]
+then
+  RUNCMD+=" -b $BINDER"
+fi
+
+if [ -n "$DATAFLOW_VERSION" ]
+then
+  RUNCMD+=" -dv $DATAFLOW_VERSION"
+fi
+
+if [ -n "$SKIPPER_VERSION" ]
+then
+  RUNCMD+=" -sv $SKIPPER_VERSION"
+fi
+
+if [ -n "$APPS_VERSION" ]
+then
+  RUNCMD+=" -av $APPS_VERSION"
+fi
+
+if [ -n "$TASKS_VERSION" ]
+then
+  RUNCMD+=" -tv $TASKS_VERSION"
+fi
+
+if [ "$SCHEDULES_ENABLED" = true]
+then
+fi
+
+CLEANCMD1+=" -t -s -d"
+CLEANCMD2+=" -t -s -d"
+
 export SPRING_CLOUD_DEPLOYER_CLOUDFOUNDRY_USERNAME=$CF_API_USERNAME
 export SPRING_CLOUD_DEPLOYER_CLOUDFOUNDRY_PASSWORD=$CF_API_PASSWORD
-export SPRING_CLOUD_DEPLOYER_CLOUDFOUNDRY_SKIP_SSL_VALIDATION=true
-export SPRING_CLOUD_DEPLOYER_CLOUDFOUNDRY_STREAM_SERVICES=rabbit
-export SPRING_CLOUD_DEPLOYER_CLOUDFOUNDRY_TASK_SERVICES=mysql
-export MYSQL_SERVICE_NAME=p-mysql
-export MYSQL_PLAN_NAME=100mb
-export RABBIT_SERVICE_NAME=p-rabbitmq
-export RABBIT_PLAN_NAME=standard
-export REDIS_SERVICE_NAME=p-redis
-export REDIS_PLAN_NAME=shared-vm
-export DEPLOY_PAUSE_TIME=10
-export TRUST_CERTS=api.sys.needles.cf-app.com
-export CF_DIAL_TIMEOUT=600
-export JAVA_BUILDPACK=java_buildpack_offline
 
 
 echo CLEANING UP RESOURCES BEFORE RUNNING TESTS
 # Cleanup resources before running the tests, do not download artifacts
 ./run.sh -p cloudfoundry -t -s -d -dv 1.6.1.BUILD-SNAPSHOT -sv 1.0.8.BUILD-SNAPSHOT || n=1
-echo FINISHED CLEAING UP RESOURCES
-# Run the tests
+echo FINISHED CLEANING UP RESOURCES
+
 echo RUNNING TESTS
+# Run the tests
 DEPLOY_PAUSE_TIME=20 ./run.sh -p cloudfoundry -b rabbit -dv 1.6.1.BUILD-SNAPSHOT -c
 echo FINISHED RUNNING TESTS
 
+
+echo CLEANING UP RESOURCES AFTER RUNNING TESTS
+./run.sh -p cloudfoundry -t -s -d
+echo FINISHED CLEANING UP RESOURCES
+
 tar -zc --ignore-failed-read --file ${repository}/spring-cloud-dataflow-acceptance-tests-${buildversion}.tar.gz target/surefire-reports
-#popd > /dev/null
+
+# with no base path, don't try to change dir
+if [ -n "$BASE_PATH" ] && popd > /dev/null
 popd > /dev/null
 
 if [ "$n" -gt 0 ]; then

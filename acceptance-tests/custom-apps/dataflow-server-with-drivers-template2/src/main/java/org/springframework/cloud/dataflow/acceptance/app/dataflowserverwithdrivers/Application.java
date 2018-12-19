ba84a7f1f209106fabd8831a5e04891940b476ac
Janne Valkealahti <janne.valkealahti@gmail.com>
2018-12-19 08:51:45 +0000

    stash

--- acceptance-tests/custom-apps/dataflow-server-with-drivers-template2/src/main/java/org/springframework/cloud/dataflow/acceptance/app/dataflowserverwithdrivers/Application.java
+++ acceptance-tests/custom-apps/dataflow-server-with-drivers-template2/src/main/java/org/springframework/cloud/dataflow/acceptance/app/dataflowserverwithdrivers/Application.java
@@ -16,11 +16,25 @@
 package org.springframework.cloud.dataflow.acceptance.app.dataflowserverwithdrivers;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.actuate.autoconfigure.security.servlet.ManagementWebSecurityAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.boot.autoconfigure.security.servlet.UserDetailsServiceAutoConfiguration;
 import org.springframework.boot.autoconfigure.session.SessionAutoConfiguration;
 import org.springframework.cloud.dataflow.server.EnableDataFlowServer;
+import org.springframework.cloud.deployer.spi.cloudfoundry.CloudFoundryDeployerAutoConfiguration;
+import org.springframework.cloud.deployer.spi.kubernetes.KubernetesAutoConfiguration;
+import org.springframework.cloud.deployer.spi.local.LocalDeployerAutoConfiguration;
 
-@SpringBootApplication(exclude = SessionAutoConfiguration.class)
+@SpringBootApplication(exclude = {
+		SessionAutoConfiguration.class,
+		ManagementWebSecurityAutoConfiguration.class,
+		SecurityAutoConfiguration.class,
+		UserDetailsServiceAutoConfiguration.class,
+		LocalDeployerAutoConfiguration.class,
+		CloudFoundryDeployerAutoConfiguration.class,
+		KubernetesAutoConfiguration.class
+})
 @EnableDataFlowServer
 public class Application {
 

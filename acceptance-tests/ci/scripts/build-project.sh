589278c8da395cbd6c7b12c67efe200a66ebdf3d
Janne Valkealahti <janne.valkealahti@gmail.com>
2018-12-21 15:23:43 +0000

    stash2

--- acceptance-tests/ci/scripts/build-project.sh
+++ acceptance-tests/ci/scripts/build-project.sh
@@ -4,6 +4,7 @@ set -e
 n=0
 source $(dirname $0)/common.sh
 repository=$(pwd)/distribution-repository
+triggers=$(pwd)/triggers
 buildversion=`date '+%Y-%m-%d-%H-%M-%S'`
 
 pushd git-repo > /dev/null
@@ -16,6 +17,7 @@ echo $ARTIFACTORY_PASSWORD | docker login -u $ARTIFACTORY_USERNAME --password-st
   -PdataflowExcludeTags="${DATAFLOW_EXCLUDE_TAGS}" \
   ${EXTRA_GRADLE_CMDLINE} \
   || n=1
+touch ${triggers}/trigger1-${buildversion}
 tar -zc --ignore-failed-read --file ${repository}/spring-cloud-dataflow-acceptance-tests-${buildversion}.tar.gz spring-cloud-dataflow-acceptance-tests/build/test-docker-logs
 popd > /dev/null
 popd > /dev/null
